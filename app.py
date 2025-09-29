4eaeae410f07b903acc338695b5eaf894f4635c4
amillet90 <amillet.iut90@gmail.com>
2025-09-29 18:34:14 +0200

    init 1 app.py fin d'installation pycharm

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, flash
+
+
 app = Flask(__name__)
+app.secret_key = 'une cle(token) : grain de sel(any random string)'
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/hello')
+def hello_world():
+    return 'Hello World!<a href="hello">lien hello</a>'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)
