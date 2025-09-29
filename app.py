b0ccb18ae1586785c0d82910dc9d42bc6752b3ff
amillet90 <amillet.iut90@gmail.com>
2025-09-29 18:40:53 +0200

    etape 1

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, redirect, flash
-
+import datetime
 
 app = Flask(__name__)
 app.secret_key = 'une cle(token) : grain de sel(any random string)'
@@ -7,8 +7,16 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route('/')
 @app.route('/hello')
-def hello_world():
-    return 'Hello World!<a href="hello">lien hello</a>'
+def hello_world():  # put application's code here
+    return 'Hello World!<a href="hello">lien hello</a>  &nbsp; <a href="/heure"> heure </a>'
+
+@app.route('/heure')
+def heure():
+    date_heure = datetime.datetime.now()
+    h = date_heure.hour
+    m = date_heure.minute
+    s = date_heure.second
+    return render_template('index_demo.html', h=h,min=m,sec=s )
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
